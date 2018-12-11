6c3e1871ef554f8babd71bb28c8008a7376351f3
caleb adams <piepieninja@gmail.com>
2018-12-11 18:25:45 -0500

    DragDetector loading as part

--- util/update_Drag_Detector.sh
+++ util/update_Drag_Detector.sh
@@ -1,9 +1,15 @@
 echo 'updating...'
 
 # move the stuff into the correct spot!
-if [ -d "$KSP/saves/Overcomplicated" ]; then
-   rm -rf "$KSP/saves/Overcomplicated"
+if [ -d "$KSP/GameData/DragDetector" ]; then
+   rm -rf "$KSP/GameData/DragDetector"
 fi
-if [ ! -d "$KSP/saves/Overcomplicated" ]; then
-  cp -r data/Overcomplicated/ "$KSP/saves/Overcomplicated"
+if [ ! -d "$KSP/GameData/DragDetector" ]; then
+  mkdir "$KSP/GameData/DragDetector"
+  mkdir "$KSP/GameData/DragDetector/Plugins"
+  mkdir "$KSP/GameData/DragDetector/Parts"
+  cp src/DragDetector/DragDetector/bin/Debug/netcoreapp2.1/DragDetector.dll "$KSP/GameData/DragDetector/Plugins/"
+  cp -r src/DragDetector/Parts/DragDetector "$KSP/GameData/DragDetector/Parts"
 fi
+
+echo 'Done!'
