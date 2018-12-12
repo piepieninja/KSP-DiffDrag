5f0473623f2e27d2187dd37bd3a8540dfe3868ab
caleb adams <piepieninja@gmail.com>
2018-12-12 15:13:53 -0500

    significant progress towards correctly detecting drag forces

---
+++ src/DragLog/DragLog/MyClass.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using UnityEngine;
+using KSP;
+
+
+//
+// super helpful link:
+// https://forum.kerbalspaceprogram.com/index.php?/topic/153765-getting-started-the-basics-of-writing-a-plug-in/
+//
+namespace HelloWorld
+{
+    //[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Hello : MonoBehaviour
+    {
+
+        // debug option for me when I'm looking at stuff in the terminal
+        bool debug = true;
+        bool verbose = true;
+        bool logActive = true;
+
+        void Awake()
+        {
+            if (verbose)
+            {
+                Debug.Log("Scene boi is awake");
+            }
+        }
+
+        public void Update(){
+            if (debug){
+                Debug.Log("Custom Script: " + Time.realtimeSinceStartup);
+            }
+
+            Vessel vess = FlightGlobals.ActiveVessel;
+
+            CelestialBody bd = FlightGlobals.currentMainBody;
+
+            Vector3d nVel = vess.srf_velocity.normalized;
+            Vector3d vLift = Vector3d.zero; // the sum of lift from all parts
+            Vector3d vDrag = Vector3d.zero; // the sum of drag from all parts
+
+            double drag = 0.0;
+            double pDrag = 0.0;
+            double pLift = 0.0;
+            double thrust = 0.0;
+            double d = vess.atmDensity;
+            double p = vess.staticPressurekPa * 1000.0;
+            double Q = vess.dynamicPressurekPa * 1000.0;
+
+
+            //Vector3d airVel = vess.orbit.GetVel() - bd.getRFrmVelOrbit(vess.orbit);//bd.getRFrmVel(position); //the velocity of the vessel relative to the air
+
+            Vector3d pos = vess.GetWorldPos3D();
+            Vector3d airVel = vess.orbit.GetVel() - bd.getRFrmVel(pos);
+
+            double airspeed = vess.indicatedAirSpeed;
+
+            if (debug){
+                Debug.Log("Density: " + d);
+                Debug.Log("Static Pressure: " + p);
+                Debug.Log("Dynamic Pressure: " + Q);
+                Debug.Log("Air Velocity: " + airVel);
+                Debug.Log("Air Vel Mag: " + airVel.magnitude);
+                Debug.Log("Air Speed: " + airspeed);
+
+            }
+
+            // calculate the bois we need
+            // loop through the parts
+            for (int i = 0; i < vess.Parts.Count; ++i)
+            {
+                Part pt = vess.Parts[i];
+                // get the part drag that is not from wing and surface
+                vDrag += -pt.dragVectorDir * pt.dragScalar;
+                if (!pt.hasLiftModule)
+                {
+                    Vector3 bodyLift = pt.transform.rotation * (pt.bodyLiftScalar * pt.DragCubes.LiftForce);
+                    bodyLift = Vector3.ProjectOnPlane(bodyLift, -pt.dragVectorDir);
+                    vLift += bodyLift;
+                }
+                // now find modules
+                for (int j = 0; j < pt.Modules.Count; ++j)
+                {
+                    var m = pt.Modules[j];
+                    if (m is ModuleLiftingSurface) // control surface derives from this
+                    {
+                        ModuleLiftingSurface wing = (ModuleLiftingSurface) m;
+                        vLift += wing.liftForce;
+                        vDrag += wing.dragForce;
+                    }
+                    // Get thrust
+                    if (m is ModuleEngines) // FX derives from this
+                    {
+                        thrust += ((ModuleEngines)m).finalThrust;
+                    }
+                }
+            }
+
+            pLift = vLift.magnitude; 
+            pDrag = vDrag.magnitude;
+            Vector3d force = vLift + vDrag; // sum of all forces on the craft
+            Vector3d liftDir = -Vector3d.Cross(vess.transform.right, nVel); // we need the "lift" direction, which
+            // is "up" from our current velocity vector and roll angle.
+            drag = Vector3d.Dot(force, -nVel); // drag force, = pDrag + lift-induced drag
+
+
+            if ((verbose || debug) && logActive){
+                Debug.Log("Drag Vector: " + vDrag);
+                Debug.Log("Drag Mag: " + vDrag.magnitude);
+                Debug.Log("Lift Vector: " + vLift);
+                Debug.Log("Lift Mag: " + vLift.magnitude);
+            }
+            if (logActive) {
+                Debug.Log("Total Froce: " + force);
+                Debug.Log("Force Mag: " + force.magnitude);
+                Debug.Log("Drag Force: " + drag);
+            }
+            //Vector3d dragForce = -0.5 * rho * FlightGlobals.DragMultiplier * part.mass * part.maximum_drag * airVel.magnitude * airVel;
+        }
+    }
+}
--- util/update_Drag_Detector.sh
+++ util/update_Drag_Detector.sh
@@ -8,8 +8,19 @@ if [ ! -d "$KSP/GameData/DragDetector" ]; then
   mkdir "$KSP/GameData/DragDetector"
   mkdir "$KSP/GameData/DragDetector/Plugins"
   mkdir "$KSP/GameData/DragDetector/Parts"
-  cp src/DragDetector/DragDetector/bin/Debug/netcoreapp2.1/DragDetector.dll "$KSP/GameData/DragDetector/Plugins/"
+  cp src/DragDetector/DragDetector/bin/Debug/DragDetector.dll "$KSP/GameData/DragDetector/Plugins/"
   cp -r src/DragDetector/Parts/DragDetector "$KSP/GameData/DragDetector/Parts"
 fi
 
+if [ -d "$KSP/GameData/DragLog" ]; then
+   rm -rf "$KSP/GameData/DragLog"
+fi
+if [ ! -d "$KSP/GameData/DragLog" ]; then
+  mkdir "$KSP/GameData/DragLog"
+  mkdir "$KSP/GameData/DragLog/Plugins"
+  mkdir "$KSP/GameData/DragDetector/Parts"
+  cp src/DragLog/DragLog/bin/Debug/DragLog.dll "$KSP/GameData/DragLog/Plugins/"
+fi
+
 echo 'Done!'
+date
